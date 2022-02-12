5d85baf39bb30825faf19e9b68d21571eaf4409a
luminaryphi <luminaryphi@protonmail.com>
2022-02-12 15:34:15 -0500

    Added about link to medium article

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
     <header>
+        <a class="head-item-left" href="https://medium.com/@TriviumNode/preserve-your-privacy-with-blackbox-f7c60b46ee46">About</a>
         <a class="head-item-right" href="https://github.com/TriviumNode">Github</a>
     </header>
 </template>
@@ -30,7 +31,17 @@ export default {
     .head-item-right {
         text-align: right;
         margin-right: 4%;
-        margin-left: 96%;
+        margin-left: 2%;
+        text-decoration: none;
+        color: whitesmoke;
+
+        
+    }
+
+    .head-item-left {
+        text-align: right;
+        float: right;
+        margin-left: 87%;
         text-decoration: none;
         color: whitesmoke;
 
